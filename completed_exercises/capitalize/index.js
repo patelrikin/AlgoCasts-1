4578eb8ce38616e6ed6ccbad5457195664207e06
Rikin <patelrikin@users.noreply.github.com>
2018-04-30 21:04:07 -0400

    Update index.js

--- completed_exercises/capitalize/index.js
+++ completed_exercises/capitalize/index.js
@@ -21,6 +21,10 @@ function capitalize(str) {
   return result;
 }
 
+function capitalizer(str) {
+  return str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+}
+
 module.exports = capitalize;
 
 // function capitalize(str) {
